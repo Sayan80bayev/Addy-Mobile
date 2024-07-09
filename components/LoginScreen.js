f317d84de8c479f1fdf5a8a3cd632b8e0dfd79d1
sayan <sayan123serv@gmail.com>
2024-07-09 16:32:24 +0500

    commit for demonst

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import { useAuthenticateUserMutation } from "../store";
 import * as SecureStore from "expo-secure-store";
 import { AlertError } from "./feedback";
@@ -28,45 +35,121 @@ const LoginScreen = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      {message && <AlertError message={message} />}
-      <Text style={styles.label}>Email</Text>
-      <TextInput
-        style={styles.input}
-        value={formData.email}
-        onChangeText={(text) => setFormData({ ...formData, email: text })}
-        placeholder="Введите email пользователя"
-      />
-      <Text style={styles.label}>Пароль</Text>
-      <TextInput
-        style={styles.input}
-        value={formData.password}
-        onChangeText={(text) => setFormData({ ...formData, password: text })}
-        placeholder="Введите пароль"
-        secureTextEntry
-      />
-      <Button title="Войти" onPress={handleLogin} />
-    </View>
+    <ScrollView contentContainerStyle={styles.main}>
+      <View style={styles.signup}>
+        <View style={styles.container}>
+          <View style={styles.signupContent}>
+            <View style={styles.formGroup}>
+              <TextInput
+                style={styles.formInput}
+                placeholder="Your Email"
+                value={formData.email}
+                onChangeText={(text) =>
+                  setFormData({ ...formData, email: text })
+                }
+              />
+            </View>
+            <View style={styles.formGroup}>
+              <TextInput
+                style={styles.formInput}
+                placeholder="Password"
+                secureTextEntry
+                value={formData.password}
+                onChangeText={(text) =>
+                  setFormData({ ...formData, password: text })
+                }
+              />
+            </View>
+            <TouchableOpacity style={styles.formSubmit} onPress={handleLogin}>
+              <Text style={styles.formSubmitText}>Sign in</Text>
+            </TouchableOpacity>
+            <Text style={styles.loginhere}>
+              Don't have an account ?{" "}
+              <Text style={styles.loginhereLink}>Sign up here</Text>
+            </Text>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  main: {
+    flexGrow: 1,
     justifyContent: "center",
-    paddingHorizontal: 16,
+    backgroundColor: "#f0f0f0",
+    paddingVertical: 20,
   },
-  label: {
-    fontSize: 18,
-    marginBottom: 8,
+
+  signup: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
   },
-  input: {
+  container: {
+    width: 660,
+    maxWidth: "90%",
+    alignItems: "center",
+  },
+  signupContent: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 50,
+    alignItems: "stretch",
+  },
+  formTitle: {
+    fontFamily: "Montserrat-Bold",
+    fontSize: 24,
+    color: "#222",
+    marginBottom: 40,
+    textTransform: "uppercase",
+    textAlign: "center",
+  },
+  formGroup: {
+    marginBottom: 20,
+  },
+  formInput: {
     borderWidth: 1,
-    borderColor: "#ccc",
-    padding: 8,
-    marginBottom: 16,
-    borderRadius: 4,
+    borderColor: "#ebebeb",
+    borderRadius: 5,
+    padding: 17,
+    fontSize: 14,
+    fontWeight: "500",
+    color: "#222",
+  },
+  labelAgreeTerm: {
+    fontSize: 12,
+    fontWeight: "600",
+    color: "#555",
+    marginLeft: 8,
+  },
+  termService: {
+    color: "#555",
+    textDecorationLine: "underline",
+  },
+  formSubmit: {
+    borderRadius: 5,
+    padding: 17,
+    alignItems: "center",
+    backgroundColor: "#ff0083",
+    marginTop: 20,
+  },
+  formSubmitText: {
+    fontSize: 14,
+    fontWeight: "700",
+    color: "#fff",
+    textTransform: "uppercase",
+  },
+  loginhere: {
+    marginTop: 20,
+    fontSize: 14,
+    color: "#555",
+    textAlign: "center",
+  },
+  loginhereLink: {
+    fontWeight: "700",
+    color: "#222",
   },
 });
-
 export default LoginScreen;
